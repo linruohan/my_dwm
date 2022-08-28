11ee26003c8f97972f31033804e6dc9a77719ab1
linruohan <mjt1220@126.com>
2022-08-28 23:35:07 +0800

    modi

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 curdir=$(dirname $(realpath $0))
-xrandr --output LVDS1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off --output VIRTUAL1 --off --output VGA-1-2 --off
 /bin/bash "${curdir}"/dwm-status.sh &
 /bin/bash "${curdir}"/wp-autochange.sh &
-picom -b
+fcitx5 &
+xrandr --output LVDS-1 --off --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal  --output VGA-1-1 --off
+feh --bg-fill --randomize ~/Pictures/guidao/*
 /bin/bash "${curdir}"/tap-to-click.sh &
 /bin/bash "${curdir}"/inverse-scroll.sh &
 /bin/bash "${curdir}"/run-mailsync.sh &
 "${curdir}"/autostart_wait.sh &
-feh --bg-fill --randomize ~/Pictures/guidao/*
--- scripts/autostart_wait.sh
+++ scripts/autostart_wait.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #xmodmap ~/.Xmodmap &
 fcitx5 &
+xrandr --output LVDS-1 --off --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal  --output VGA-1-1 --off
+picom -b
+feh --bg-fill --randomize ~/Pictures/guidao/*
